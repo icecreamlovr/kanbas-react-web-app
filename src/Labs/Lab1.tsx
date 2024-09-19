b0641a6ff7f969e9215292364c3e8c237efaf3fd
Yue (Claire) He <115207213+icecreamlovr@users.noreply.github.com>
2024-09-19 02:14:30 -0700

    Add a few details per assignment requirements

--- src/Labs/Lab1.tsx
+++ src/Labs/Lab1.tsx
@@ -1,3 +1,13 @@
 export default function Lab1() {
-  return <h2>Lab 1</h2>;
+  return (
+    <div>
+      <a
+        href="https://docs.google.com/document/d/1O_Vyk-9fNEBVYoEH_oFrhDcDTVJufhmjv3NzXJqwo44/edit"
+        id="wd-github"
+        target="_blank"
+      >
+        <h2>Lab 1</h2>
+      </a>
+    </div>
+  );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,7 +7,8 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
-      <h2>Student: Yue (Claire) He</h2>
+      <h2>Student: Yue (Claire) He (Sec03)</h2>
+
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
