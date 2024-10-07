c196b24d9e7f72cd58bde7194bdf87afe1a3b66e
Yue (Claire) He <115207213+icecreamlovr@users.noreply.github.com>
2024-10-07 16:06:02 -0700

    Commit Lab1 content.

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,10 +2,11 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
+import Lab3 from "./Lab3";
 
 export default function Labs() {
   return (
-    <div>
+    <div id="wd-labs">
       <h1>Labs</h1>
       <h2>Student: Yue (Claire) He (Sec03)</h2>
 
@@ -14,7 +15,7 @@ export default function Labs() {
         <Route path="/" element={<Navigate to="Lab1" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3" element={<h2>Lab3</h2>} />
+        <Route path="Lab3" element={<Lab3 />} />
       </Routes>
     </div>
   );
