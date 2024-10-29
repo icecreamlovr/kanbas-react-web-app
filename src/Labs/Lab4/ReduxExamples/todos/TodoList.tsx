2b116748a73e2a6cf59a79ef220cb27df7830274
Yue (Claire) He <115207213+icecreamlovr@users.noreply.github.com>
2024-10-29 15:24:33 -0700

    Add Lab4

---
+++ src/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
@@ -0,0 +1,19 @@
+import TodoItem from "./TodoItem";
+import TodoForm from "./TodoForm";
+import { useSelector } from "react-redux";
+
+export default function TodoList() {
+  const { todos } = useSelector((state: any) => state.todosReducer);
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <ul className="list-group">
+        <TodoForm />
+        {todos.map((todo: any) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+      <hr />
+    </div>
+  );
+}
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,20 +3,25 @@ import TOC from "./TOC";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
+import { Provider } from "react-redux";
+import store from "./store";
 
 export default function Labs() {
   return (
-    <div id="wd-labs">
-      <h1>Labs</h1>
-      <h2>Student: Yue (Claire) He (Sec 03)</h2>
+    <Provider store={store}>
+      <div id="wd-labs" className="container-fluid">
+        <h1>Student: Yue (Claire) He (Sec 03)</h1>
+        <h1>Labs</h1>
 
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3/*" element={<Lab3 />} />
-      </Routes>
-    </div>
+        <TOC />
+        <Routes>
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2" element={<Lab2 />} />
+          <Route path="Lab3/*" element={<Lab3 />} />
+          <Route path="Lab4/*" element={<Lab4 />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
